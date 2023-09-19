35d0046f36a837fa9c5d892fa77f8ef6a1642ef3
in23mb <94417405+in23mb@users.noreply.github.com>
2023-09-19 14:12:21 -0300

    Add files via upload

---
+++ data_includes/meu_script.js
@@ -0,0 +1,127 @@
+PennController.ResetPerfix(null);
+
+Sequence("Participante", "Intrucoes", randomize("Experimento"), SendResults(),"Final")
+
+Header(
+
+  defaultText
+   .css("font-size", "1.2em")
+   .print()
+  , 
+
+  defaultTextImput
+   .css("font-size", "1.2em")
+   .print()
+  ,
+
+  defaultTextButtom
+   .css("font-size", "1.2em")
+   .print()
+   .center()
+   .wait()
+  
+)
+
+newtrial("Participante",
+
+        newText("<p>Bem-Vindos</p>")
+        ,
+        newText("<p>Neste experimento, você vai ouvir um frase e depois deve escolher a melhoropção da interpretação para ela.</p>")
+        ,
+
+        newText("<p>Por favor, escreva seu NOME COMPLETO na caixa abaixo</p>")
+        ,
+        newTextImput("Nome")
+        ,
+
+        newText("<p>Agora selecione sua ESCOLARIDADE na caixa abaixo e aperte o botão 'Iniciar' para começar</p>")
+        ,
+        NewDropDown("Escolaridade", "Selecione sua escolaridade")
+           .add("Médio completo", "Superior em Curso", "Superior completo", "Pós--Graduação")
+           .css("font-size", "1.2em")
+           .print()
+           .log()
+        , 
+
+        newButton("Iniciar")
+        ,
+
+        newVar("NOME")
+           .global()
+           .set( getTextInput("Nome") )
+  
+)
+
+.log("NOME"  , getVar("NOME") )
+
+newtrial("Instrucoes",
+        
+         newText("<p>INSTRUÇÕES</p>")
+         ,
+         newText("<p>Ouça a frase com atenção e depois clique em cima de uma sentenças, <strong>A</strong> ou <strong>B</strong></p>")
+         ,
+
+         newButton("Iniciar")
+            .log()
+           
+)
+
+Template("tabela_scrippt_auditivo.csv", 
+         
+         row => newTrial( "Experimento",
+
+                         newAudio("AudioExperimento", row.AudioExperimento)
+                            .play()
+                         ,
+                         
+                        newImage("alto_falante_icone.png")
+                            .size( 90 , 90 )
+                            .print()
+                        ,
+       
+                        newButton("Próximo")
+                            .log()
+                            .remove()
+                        ,
+                          
+                        getImage("alto_falante_icone.png")
+                            .remove()
+                        ,
+                         
+        
+                        newText("A",row.SentencaA)
+                        ,
+                        newText("B",row.SentencaB)
+                        ,
+                         
+                        newCanvas( "2000vw", "800vh" )
+                            .add( "center at 25%" , "middle at 2%" , getText("A") )
+                            .add( "center at 75%" , "middle at 2%" , getText("B") )
+                            .print() 
+                        ,
+                         
+                        newSelector()
+                            .add( getText("A") , getText("B") )
+                            .keys("A","B")
+                            .log()
+                            .wait()
+               )
+         
+        .log("Group", row.Group)
+        .log("Item", row.Item)
+)
+
+newTrial( "Final" ,
+         
+        newText("<p> O experimento foi concluído. Obrigada pela participação!</p>")
+        .center()
+        ,
+        newText("<p> Você receberá um e-mail com a sua declaração de participação.</p>")
+        .center()
+        .wait()
+
+         
+ )
+
+
+.setOption("countsForProgressBar",false);
